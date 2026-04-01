7418fdef0d3ce8a9d25d8e85be5864fa9b98f567
William Roshandel <williamsaeed15@gmail.com>
2026-03-31 22:34:27 -0500

    Time: 18 ms (54.84%) Space: 12.4 MB (74.19%)

---
+++ 3617-find-the-original-typed-string-i/find-the-original-typed-string-i.py
@@ -0,0 +1,14 @@
+class Solution:
+    def possibleStringCount(self, word):
+        """
+        :type word: str
+        :rtype: int
+        """
+        n = len(word)
+        count  = n
+        for i in range(1,n):
+            if word[i] != word[i-1]:
+                count -= 1
+        return count
+
+        
