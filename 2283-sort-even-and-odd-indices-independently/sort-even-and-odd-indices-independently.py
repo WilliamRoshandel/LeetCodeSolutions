dd7ff7896c67e90a4f39e8ee8c859fc2dee30758
William Roshandel <williamsaeed15@gmail.com>
2026-05-18 13:49:55 -0500

    Time: 4 ms (35.06%) Space: 12.5 MB (20.13%)

--- 2283-sort-even-and-odd-indices-independently/sort-even-and-odd-indices-independently.py
+++ 2283-sort-even-and-odd-indices-independently/sort-even-and-odd-indices-independently.py
@@ -1,28 +1,28 @@
 class Solution:
     def sortEvenOdd(self, nums):
-        even = [0] * 101
-        odd = [0] * 101
+        
+        odds = [0] * 101
+        evens = [0] * 101
         n = len(nums)
 
-        for i in range (0, n, 2):
-            even[nums[i]] += 1
-
         for i in range (1, n, 2):
-            odd[nums[i]] += 1
-        
+            odds[nums[i]] += 1
+        for i in range (0, n, 2):
+            evens[nums[i]] += 1
+
         k = 0
-        for i in range(101):
-            while even[i] > 0:
+        for i in range (101):
+            while evens[i] > 0:
                 nums[k] = i
-                k += 2
-                even[i] -= 1
+                k += 2 
+                evens[i] -= 1
 
         k = 1
-        for i in range(100, -1, -1):
-            while odd[i] > 0:
+        for i in range (100, -1, -1):
+            while odds[i] > 0:
                 nums[k] = i
-                k += 2
-                odd[i] -= 1
+                k += 2 
+                odds[i] -= 1
+
 
         return nums
-        
