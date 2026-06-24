65559d088dcd619f29d752f8743b48801aee18a2
William Roshandel <williamsaeed15@gmail.com>
2026-06-23 20:05:28 -0500

    Time: 18 ms (63.93%) Space: 25.9 MB (56.31%)

---
+++ 0217-contains-duplicate/contains-duplicate.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        hashSet = set()
+
+        for n in nums:
+            if n in hashSet:
+                return True
+            hashSet.add(n)
+        return False
+        
