771d10ac30a71821fde774d2d13045909bc7cf6f
William Roshandel <williamsaeed15@gmail.com>
2026-06-29 13:22:53 -0500

    Time: 3 ms (63.55%) Space: 15 MB (70.65%)

--- 0104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.py
+++ 0104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.py
@@ -6,10 +6,19 @@
 #         self.right = right
 class Solution(object):
     def maxDepth(self, root):
-        if not root:
-            return 0
+        stack = [[root, 1]]
+        result = 0
 
-        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+        while stack:
+            node, depth = stack.pop()
+
+            if node:
+                result = max(result, depth)
+                stack.append([node.left, depth + 1])
+                stack.append([node.right, depth + 1])
+                
+        return result
+                
 
             
         
