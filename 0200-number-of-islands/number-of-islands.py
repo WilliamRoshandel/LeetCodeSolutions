5a9f570ad43a379f2273cfaf71d9eff723f93ad0
William Roshandel <williamsaeed15@gmail.com>
2026-07-06 16:25:25 -0500

    Time: 1513 ms (5.01%) Space: 31.2 MB (17.51%)

--- 0200-number-of-islands/number-of-islands.py
+++ 0200-number-of-islands/number-of-islands.py
@@ -13,7 +13,7 @@ class Solution(object):
             q.append((r, c))
 
             while q:
-                row, col = q.popleft()
+                row, col = q.pop()
                 directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
 
                 for dr, dc in directions:
